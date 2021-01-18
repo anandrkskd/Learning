5617416d59d2ce6702867f67e94bcc04939ee010
anandrkskd <anandrkskd@gmail.com>
2021-01-18 15:41:12 +0530

    adding files

---
+++ docker-compose/docker-with-jenkins/A.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [5]int64{100, 20, 10, 5, 1}
+	var input, noOfBill int64
+	fmt.Scanf("%d", &input)
+	for _, j := range a {
+		if input/j > 0 && input%j == 0 {
+			noOfBill = noOfBill + input/int64(j)
+			break
+		} else if input/j > 0 {
+			noOfBill = noOfBill + input/int64(j)
+		}
+		input = input % j
+	}
+	fmt.Println(noOfBill)
+}
