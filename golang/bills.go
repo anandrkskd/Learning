dbf803ad6d81e473a8397e942d8639543f8ea8ed
anandrkskd <anandrkskd@gmail.com>
2021-01-19 12:21:28 +0530

    add improving bill.go

--- golang/bills.go
+++ golang/bills.go
@@ -6,6 +6,10 @@ func main() {
 	a := [5]int64{100, 20, 10, 5, 1}
 	var input, noOfBill int64
 	fmt.Scanf("%d", &input)
+	if input < 5 {
+		fmt.Println(input)
+		return
+	}
 	for _, j := range a {
 		if input/j > 0 && input%j == 0 {
 			noOfBill = noOfBill + input/int64(j)
